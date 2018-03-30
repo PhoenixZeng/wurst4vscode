3aec0422f9d6cbf39af792a68a9e69f478a66d7a
Phoenix <zengphoenix@live.com>
2018-03-30 16:59:26 +0800

    add custom command

--- src/features/commands.ts
+++ src/features/commands.ts
@@ -73,6 +73,44 @@ export function registerCommands(client: LanguageClient): vscode.Disposable {
 		_lastMapConfig = mappath
 		return client.sendRequest(ExecuteCommandRequest.type, request)
 	};
+	let custom_command = async (args: any[]) => {
+		let config = vscode.workspace.getConfiguration("wurst");
+		let apppath = config.get<string>("apppath");
+		let command = config.get<string>("command");
+		if (!apppath) {
+			return Promise.reject("apppath not set (change 'wurst.apppath' in your settings).");
+		}
+		if (!command) {
+			return Promise.reject("command not set (change 'wurst.command' in your settings).");
+		}
+		let mapPromise: Thenable<string>;
+		if (args && args.length > 0) {
+			mapPromise = Promise.resolve(args[0]);
+		} else {
+			let items = workspace.findFiles('*.w3x', null, 10)
+				.then(uris => uris.sort(function(a, b) {
+					return fs.statSync(b.fsPath).mtime.getTime() -
+							fs.statSync(a.fsPath).mtime.getTime();
+				}))
+				.then(uris => uris.map(uri => uri.path))
+			mapPromise = window.showQuickPick(items)
+		}
+		let mappath = await mapPromise;
+		if (!mappath) {
+			return Promise.reject("No map selected.");
+		}
+
+		let request: ExecuteCommandParams = {
+			command: "wurst.custom_command",
+			arguments: [{
+				'mappath': mappath,
+				'wc3path': wc3path,
+				'command': command
+			}]
+		};
+		_lastMapConfig = mappath
+		return client.sendRequest(ExecuteCommandRequest.type, request)
+	};
 
 	let startLast = () => {
 		if (_lastMapConfig) {
@@ -114,6 +152,7 @@ export function registerCommands(client: LanguageClient): vscode.Disposable {
 		//vscode.commands.registerCommand('wurst.restart', () => client.restart()),
 		// vscode.commands.registerCommand('wurst.clean', () => client.clean()),
 		vscode.commands.registerCommand('wurst.startmap', (args: any[]) => startMap(args)),
+		vscode.commands.registerCommand('wurst.custom_command', (args: any[]) => custom_command(args)),
 		vscode.commands.registerCommand('wurst.startlast', () => startLast()),
 		vscode.commands.registerCommand('wurst.buildmap', (args: any[]) => buildMap(args)),
 		vscode.commands.registerCommand('wurst.tests', () => tests('all')),
